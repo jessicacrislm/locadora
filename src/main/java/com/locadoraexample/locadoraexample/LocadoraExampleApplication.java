ef47490707e365f2008d1fca6117939858761887
Jessica Machado <jessica-cristina@ultracar.com.br>
2020-04-24 16:29:47 -0300

    first-commit

---
+++ src/main/java/com/locadoraexample/locadoraexample/LocadoraExampleApplication.java
@@ -0,0 +1,13 @@
+package com.locadoraexample.locadoraexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locadoraexample/locadoraexample/LocadoraExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locadoraexample.locadoraexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
